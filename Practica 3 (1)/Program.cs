43378612a18141176d9cc6d7dc188de5c1ad72fc
Arov752 <78989365+Arov752@users.noreply.github.com>
2021-02-17 14:09:58 -0400

    Add files via upload

---
+++ Practica 3 (1)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practica_3__1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0;
+            int b=1;
+
+            while (b > 0)
+            {
+                Console.WriteLine("escriba un numero mayor a 0");
+                b = int.Parse(Console.ReadLine());
+                a = a + b;
+                Console.Clear();
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ Practica 3 (2)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practica_3__2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int a=0;
+            do
+            {
+                
+                a = a+ 1;
+                Console.WriteLine(a);
+                Console.ReadKey();
+            }
+
+            while (a <= 9);
+        }
+    }
+}
